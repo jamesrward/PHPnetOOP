ff5c1be627f24c96d932594f68801556e0e100fd
James Ward <jamesroderickward@gmail.com>
2016-03-31 15:32:36 -0400

    Class Constants Example #2

---
+++ Class Constants/ex3.2.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: James
+ * Date: 2016-03-31
+ * Time: 3:30 PM
+ */
+class foo {
+  // As of php 5.3.0
+  const BAR = <<<'EOT'
+bar
+EOT;
+
+  // As of php 5.3.0
+  const BAZ = <<<EOT
+baz
+EOT;
+
+};
